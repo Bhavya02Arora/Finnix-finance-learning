65d9e187dbb95fe2f1d466eec7685f271816ab88
Bhavya Arora <bhavya789arora@gmail.com>
2025-03-01 20:22:08 -0500

    learn page and side bar

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,30 @@
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { UserProgress } from "@/components/user-progress";
+import { Header } from "./header";
+
+
 
 const LearnPage = () => {
     return (
-        <div>
-            <h1>Learn</h1>
-            <p>Welcome to the learning page!</p>
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            {/* <h1>Learn</h1>
+            <p>Welcome to the learning page!</p> */}
+            <StickyWrapper>
+                <UserProgress
+                    activeCourse={{title: "Exchange-traded funds", imageSrc: "/etf.jpg"}}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                />
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Exchange-traded funds" />
+                {/* <Header title="Stocks" />
+                <Header title="Mutual funds" />
+                <Header title="Bonds" /> */}
+            </FeedWrapper>
+            
         </div>
     );
 };
